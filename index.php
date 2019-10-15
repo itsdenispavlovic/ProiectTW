dcd99327bb89376681bdf88d7bcbbb35dc1b7896
itsdenispavlovic <pavlovicdenis@icloud.com>
2019-10-15 17:19:39 +0300

    Added counter on newsfeed post

--- index.php
+++ index.php
@@ -20,11 +20,26 @@ if(!$user->isLoggedin())
         <hr>
         <h1>Newsfeed</h1>
         <form id="ssForm">
-        <textarea name="writeaPost" id="post" cols="70" rows="6" style="width: 100%; border: 0.5px solid black;resize: none; border-radius: 20px;" ></textarea>
+        <textarea name="writeaPost" id="post" onkeydown="countChar(this)" onkeyup="countChar(this)" max="1000" maxlength="1000" cols="70" rows="6" style="width: 100%; border: 0.5px solid black;resize: none; border-radius: 20px;" spellcheck="false"></textarea>
         <input type="text" name="uid" hidden value="<?php echo $uid; ?>"/>
+        <span style="float: left;" id="counter"></span>
         <input type="submit" id="writePost" style="float: right" class="btn btn-info" value="POST">
         </form>
-
+        <script>
+          $('#counter').text(1000);
+          function countChar(val)
+          {
+            var len = val.value.length;
+            if(len >= 1000)
+            {
+              val.value.substring(0, 1000);
+            }
+            else
+            {
+              $('#counter').text(1000-len);
+            }
+          };
+        </script>
         <div class="row">
           <!-- Space -->
           <br />
