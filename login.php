2f09e6622e006deb016fa0936ee3522096028117
itsdenispavlovic <pavlovicdenis@icloud.com>
2019-10-22 22:56:06 +0300

    Login Version 2

--- login.php
+++ login.php
@@ -1,87 +1,150 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Login</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- Custom Theme files -->
-<link href="css/regDesign.css" rel="stylesheet" type="text/css" media="all" />
-<!-- //Custom Theme files -->
-<!-- web font -->
-<link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
-<!-- //web font -->
-
+	<title>Login into Bookface</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <body>
-	<!-- main -->
-	<div class="main-w3layouts wrapper">
-		<h1>A simple login page</h1>
-		<div class="main-agileinfo">
-			<div class="agileits-top">
-				<form id="loginForm">
-					<input class="text" type="text" name="username" placeholder="Username" required="">
-					<!-- <input class="text email" type="email" name="email" placeholder="Email" required=""> -->
-					<input class="text" type="password" name="password" placeholder="Password" required="">
-					<!-- <input class="text w3lpass" type="password" name="password" placeholder="Confirm Password" required=""> -->
-					<!-- <div class="wthree-text">
-						<label class="anim">
-							<input type="checkbox" class="checkbox" required="">
-							<span>I Agree To The Terms & Conditions</span>
-						</label>
-						<div class="clear"> </div>
-					</div> -->
-                    <input id="logB" class="btn btn-info" type="submit" value="Login">
-                    
-                    <hr>
-				</form>
-				<p>Don't have an Account? <a href="registration.php"> Register Now!</a></p>
+	
+	<div class="limiter">
+		<div class="container-login100">
+		<!-- Bootstrap Modal  -->
+		<div class="modal fade" id="LoginError" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+				<div class="modal-header bg-danger">
+					<h5 class="modal-title text-light" id="exampleModalLabel">Wrong Credentials</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body text-center">
+					You have entered wrong Credentials! Please try again.
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				</div>
+				</div>
 			</div>
 		</div>
-		<div class="colorlibcopy-agile">
-			<p>© 2019 Proiect TW</p>
+		<!-- End of bootstrap modal  -->
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-50">
+				<form id="loginForm" class="login100-form validate-form">
+					<span class="login100-form-title p-b-33">
+						Account Login
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Username is required">
+						<input class="input100" type="text" name="username" placeholder="Username">
+						<span class="focus-input100-1"></span>
+						<span class="focus-input100-2"></span>
+					</div>
+
+					<div class="wrap-input100 rs1 validate-input" data-validate="Password is required">
+						<input class="input100" type="password" name="password" placeholder="Password">
+						<span class="focus-input100-1"></span>
+						<span class="focus-input100-2"></span>
+					</div>
+
+					<div class="container-login100-form-btn m-t-20">
+						<button id="logB" class="login100-form-btn" type="submit">
+							Sign in
+						</button>
+					</div>
+					<div class="text-center p-t-45 p-b-4">
+                        <!--
+                        <span class="txt1">
+							Forgot
+						</span>
+
+						 <a href="#" class="txt2 hov1">
+							Username / Password?
+						</a> -->
+					</div>
+					<div class="text-center">
+						<span class="txt1">
+							Create an account?
+						</span>
+
+						<a href="#" class="txt2 hov1">
+							Sign up
+						</a>
+					</div>
+				</form>
+			</div>
 		</div>
-		<ul class="colorlib-bubbles">
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-		</ul>
 	</div>
-    <!-- //main -->
     
     <script>
-            $(document).ready(() => {
-                $('#logB').click((e) => {
-                    e.preventDefault();
-        
-                    $.ajax
-                    ({
-                        type: 'POST',
-                        url: 'logC.php',
-                        data: $('#loginForm').serialize(),
-                        success: (response) => {
-                            // alert(response);
-                            if(response == 1)
-                            {
-                                // alert("Is now logged in!");
-                                // If we move to server, change it on /
-                                location.href = "index.php";
-                            }
-                        }
-                    });
-                });
+    $(document).ready(() => {
+        $('#logB').click((e) => {
+            e.preventDefault();
+
+            $.ajax
+            ({
+                type: 'POST',
+                url: 'logC.php',
+                data: $('#loginForm').serialize(),
+                success: (response) => {
+                    // alert(response);
+                    if(response == 1)
+                    {
+                        // alert("Is now logged in!");
+                        // If we move to server, change it on /
+                        location.href = "index.php";
+                    }
+					else
+					{
+						$('#LoginError').modal('show');
+						// alert(response);
+					}
+                }
             });
-            </script>
+        });
+    });
+    </script>
 
-
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
 
 </body>
 </html>
